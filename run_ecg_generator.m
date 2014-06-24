ea389ef1b55c570530c0cc58d54b5a7dc254eba3
Fernando Andreotti <fernando.andreotti@gmx.de>
2014-06-24 14:57:50 +0100

    Removed reference electrode from output mixture

--- run_ecg_generator.m
+++ run_ecg_generator.m
@@ -340,9 +340,13 @@ disp('Projecting dipoles...')
 % % % % using mean body potential as reference (ground electrode)
 ground = mixture(end,:);
 mixture = mixture(1:end-1,:) - repmat(ground,size(mixture,1)-1,1);
+mixture(end,:) = [];
 mecg = mecg(1:end-1,:);
+mecg(end,:) = [];
 fecg = cellfun(@(x) x(1:end-1,:),fecg,'UniformOutput',0);
+fecg(end,:) = [];
 noise = cellfun(@(x) x(1:end-1,:),noise,'UniformOutput',0);
+noise(end,:) = [];
 vols.refpos = param.refpos;
 vols.elpos = vols.elpos(1:end-1,:); % removing ground electrode
 % == FORMATING OUTPUT ARGUMENTS

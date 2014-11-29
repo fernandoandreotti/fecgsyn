21992157e43aa70ce8f35d786f54acbbb57d9b47
Fernando Andreotti <fernando.andreotti@gmx.de>
2014-11-29 17:39:36 +0100

    Fixed for new 1-min blocks extraction for TS/AM techniques

--- morphological_analysis/FECGSYN_genresults.m
+++ morphological_analysis/FECGSYN_genresults.m
@@ -1,8 +1,8 @@
 function FECGSYN_genresults(path_orig,path_ext,fs,ch)
 % Input:
-%  path_orig:       Path for original dataset
-%  path_ext:        Path for extracted dataset
-%  fs:              Sampling frequency
+% path_orig: Path for original dataset
+% path_ext: Path for extracted dataset
+% fs: Sampling frequency
 % this script generates a series of abdominal mixtures, containing i) a
 % stationary case and ii) non-stationary case (when adding breathing
 % effects, foetal movement etc.).
@@ -11,7 +11,7 @@ function FECGSYN_genresults(path_orig,path_ext,fs,ch)
 % NI-FECG simulator toolbox, version 1.0, February 2014
 % Released under the GNU General Public License
 %
-% Copyright (C) 2014  Joachim Behar & Fernando Andreotti
+% Copyright (C) 2014 Joachim Behar & Fernando Andreotti
 % Oxford university, Intelligent Patient Monitoring Group - Oxford 2014
 % joachim.behar@eng.ox.ac.uk, fernando.andreotti@mailbox.tu-dresden.de
 %
@@ -24,27 +24,27 @@ function FECGSYN_genresults(path_orig,path_ext,fs,ch)
 %
 % This program is distributed in the hope that it will be useful,
 % but WITHOUT ANY WARRANTY; without even the implied warranty of
-% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 % GNU General Public License for more details.
 %
 % You should have received a copy of the GNU General Public License
-% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+% along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 global debug
 debug = 1;
 
 %% == Parameters
-INTERV = round(0.05*fs);    % BxB acceptance interval
-TEMP_SAMPS = round(60*fs);    % samples used for building templates
-morph = 1;                  % turn on/off morphological analysis
+INTERV = round(0.05*fs); % BxB acceptance interval
+TEMP_SAMPS = round(60*fs); % samples used for building templates
+morph = 0; % turn on/off morphological analysis
 %% Run through extracted datasets
 cd(path_orig)
 slashchar = char('/'*isunix + '\'*(~isunix));
-fls_orig = dir('*.mat');     % looking for .mat (creating index)
-fls_orig =  arrayfun(@(x)x.name,fls_orig,'UniformOutput',false);
+fls_orig = dir('*.mat'); % looking for .mat (creating index)
+fls_orig = arrayfun(@(x)x.name,fls_orig,'UniformOutput',false);
 cd(path_ext)
-fls_ext = dir('*.mat');     % looking for .mat (creating index)
-fls_ext =  arrayfun(@(x)x.name,fls_ext,'UniformOutput',false);
+fls_ext = dir('*.mat'); % looking for .mat (creating index)
+fls_ext = arrayfun(@(x)x.name,fls_ext,'UniformOutput',false);
 
 stats_ica = zeros(length(fls_orig),4);
 stats_pca = zeros(length(fls_orig),4);
@@ -59,7 +59,8 @@ morph_ica = zeros(length(fls_orig),2);
 morph_tspca = zeros(length(fls_orig),2);
 morph_tskf = zeros(length(fls_orig),2);
 morph_aesn = zeros(length(fls_orig),2);
-
+idx = cellfun(@(x) strcmp(x(1:3),'rec'),fls_ext);
+fls_ext(~idx) = [];
 
 for i = 1:length(fls_ext)
     disp(fls_ext{i})
@@ -75,7 +76,6 @@ for i = 1:length(fls_ext)
     if exist('outdata','var') % uniform naming residuals
         residual = outdata;
     end
-    
     %= Resampling data (if necessary)
     if size(out.mecg,2) ~= size(residual,2)
         fref = floor(out.fqrs{1}.*(size(residual,2)/size(out.mecg,2)));
@@ -90,17 +90,16 @@ for i = 1:length(fls_ext)
     [elif,~]=strtok(file{:}(end:-1:1),slashchar);
     disp(elif(end:-1:1))
     clear fecg outdata rec file elif k
-    
     switch met{:}(2:end-4)
         case 'JADEICA'
             % generating statistics
             [F1,MAD,PPV,SE] = Bxb_compare(fref,fqrs,INTERV);
             stats_ica(origrec,:) = [F1,MAD,PPV,SE];
             if morph
-                [qt_err,theight_err]=BSS_morpho(fecgref,residual,fref,maxch,TEMP_SAMPS);
+                [qt_err,theight_err]=morpho(fecgref,residual,fref,maxch,TEMP_SAMPS);
+                morph_ica(origrec,:) = [mean(qt_err); mean(theight_err)];
+                print('-dpng','-r72',[cd '/plots/' fls_ext{i} '.png'])
             end
-            morph_ica(origrec,:) = [mean(qt_err); mean(theight_err)];
-            print('-dpng','-r72',[cd '/plots/' fls_ext{i} '.png'])
             clear fqrs F1 MAD PPV SE
         case 'PCA'
             %= generating statistics
@@ -108,84 +107,54 @@ for i = 1:length(fls_ext)
             stats_pca(origrec,:) = [F1,MAD,PPV,SE];
             clear fqrs F1 MAD PPV SE
         case 'tsc'
-            %= discarding channels that are not the best
-            fqrs = fqrs{maxch};
-            residual = double(residual(maxch,:))./3; %in mV
-            fecg = double(out.fecg{1}(ch(maxch),:))./3; %in mV
             %= generating QRS detection statistics
             [F1,MAD,PPV,SE] = Bxb_compare(fref,fqrs,INTERV);
             stats_tsc(origrec,:) = [F1,MAD,PPV,SE];
-            
             clear fqrs F1 MAD PPV SE
-            
         case 'tspca'
-            %= discarding channels that are not the best
-            fqrs = fqrs{maxch};
-            residual = residual(maxch,:);
-            fecgref = fecgref(maxch,:);
             %= generating statistics
             [F1,MAD,PPV,SE] = Bxb_compare(fref,fqrs,INTERV);
             stats_tspca(origrec,:) = [F1,MAD,PPV,SE];
-            
             %= morphological statistics
             if morph
-                [qt_err,theight_err]=TS_morpho(fecgref,residual,fref,TEMP_SAMPS);
+                [qt_err,theight_err]=morpho(fecgref,residual,fref,maxch,TEMP_SAMPS);
+                morph_tspca(origrec,:) = [mean(qt_err); mean(theight_err)];
+                print('-dpng','-r72',[cd '/plots/' fls_ext{i} '.png'])
             end
-            morph_tspca(origrec,:) = [mean(qt_err); mean(theight_err)];
-            print('-dpng','-r72',[cd '/plots/' fls_ext{i} '.png'])
-            
             clear fecg residual fqrs F1 MAD PPV SE qt_err theight_err
         case 'tsekf'
-            %= discarding channels that are not the best
-            fqrs = fqrs{maxch};
-            residual = residual(maxch,:);
-            fecgref = fecgref(maxch,:);
-            %= generating statistics
+            % generating statistics
             [F1,MAD,PPV,SE] = Bxb_compare(fref,fqrs,INTERV);
             stats_tsekf(origrec,:) = [F1,MAD,PPV,SE];
-            
             %= morphological statistics
             if morph
-                [qt_err,theight_err]=TS_morpho(fecgref,residual,fref,TEMP_SAMPS);
+                [qt_err,theight_err]=morpho(fecgref,residual,fref,maxch,TEMP_SAMPS);
+                morph_tskf(origrec,:) = [mean(qt_err); mean(theight_err)];
+                print('-dpng','-r72',[cd '/plots/' fls_ext{i} '.png'])
             end
-            morph_tskf(origrec,:) = [mean(qt_err); mean(theight_err)];
-            print('-dpng','-r72',[cd '/plots/' fls_ext{i} '.png'])
             clear fecg residual fqrs F1 MAD PPV SE qt_err theight_err
         case 'alms'
-            %= discarding channels that are not the best
-            fqrs = fqrs{maxch};
-            residual = residual(maxch,:);
             % generating statistics
             [F1,MAD,PPV,SE] = Bxb_compare(fref,fqrs,INTERV);
             stats_alms(origrec,:) = [F1,MAD,PPV,SE];
             clear fecg residual fqrs F1 MAD PPV SE
         case 'arls'
-            % discarding channels that are not the best
-            fqrs = fqrs{maxch};
-            residual = residual(maxch,:);
             % generating statistics
             [F1,MAD,PPV,SE] = Bxb_compare(fref,fqrs,INTERV);
             stats_arls(origrec,:) = [F1,MAD,PPV,SE];
             clear fecg residual fqrs F1 MAD PPV SE
         case 'aesn'
-            % discarding channels that are not the best
-            fqrs = fqrs{maxch};
-            residual = residual(maxch,:);
-            fecgref = fecgref(maxch,:);
             % generating statistics
             [F1,MAD,PPV,SE] = Bxb_compare(fref,fqrs,INTERV);
             stats_aesn(origrec,:) = [F1,MAD,PPV,SE];
-            
             % morphological statistics
             if morph
-                [qt_err,theight_err]=TS_morpho(fecgref,residual,fref,TEMP_SAMPS);
+                [qt_err,theight_err]=morpho(fecgref,residual,fref,maxch,TEMP_SAMPS);
+                morph_aesn(origrec,:) = [mean(qt_err); mean(theight_err)];
+                print('-dpng','-r72',[cd '/plots/' fls_ext{i} '.png'])
             end
-            morph_aesn(origrec,:) = [mean(qt_err); mean(theight_err)];
-            print('-dpng','-r72',[cd '/plots/' fls_ext{i} '.png'])
             clear fecg residual fqrs F1 MAD PPV SE
-
     end
-    
     clear fecg residual fqrs F1 MAD PPV SE qt_err theight_err
 end
 
@@ -216,19 +185,19 @@ ylabel('MAD (ms)','FontSize',FSIZE)
 
 end
 
-function [qt_err,theight_err]=BSS_morpho(fecg,residual,fqrs,maxch,SAMPS)
-%% Function to perform morphological analysis for BSS extracted data
+function [qt_err,theight_err]=morpho(fecg,residual,fqrs,maxch,SAMPS)
+%% Function to perform morphological analysis for TS/BSS extracted data
 %
 % >Inputs
-%  fecg:        Propagated fetal signal before mixture with noise sources
-%  residual:    Result of fetal extraction from abdominal signals
-%  fqrs:        Reference fetal QRS samplestamps
-%  maxch:       Channel with highest F1-accuracy for FQRS detections
-%  SAMPS:       Number of samples used for generating templates
+% fecg: Propagated fetal signal before mixture with noise sources
+% residual: Result of fetal extraction from abdominal signals
+% fqrs: Reference fetal QRS samplestamps
+% maxch: Channel with highest F1-accuracy for FQRS detections
+% SAMPS: Number of samples used for generating templates
 %
 % > Outputs
-%  qt_err:      Array containing QT error for each template
-%  theight_err: Array containing T-height error for each template
+% qt_err: Array containing QT error for each template
+% theight_err: Array containing T-height error for each template
 %
 
 % generating reference template
@@ -253,46 +222,9 @@ for j = 1:SAMPS:length(residual)
     % evaluating morphological features
     [qt_err(block),theight_err(block)] = FECGSYN_manalysis(temp_abdm,temp_ref);
     block = block+1;
-    
 end
 end
 
-function [qt_err,theight_err]=TS_morpho(fecg,residual,fqrs,SAMPS)
-%% Function to perform morphological analysis for TS extracted data
-%
-% >Inputs
-%  fecg:        Propagated fetal signal before mixture with noise sources
-%  residual:    Result of fetal extraction from abdominal signals
-%  fqrs:        Reference fetal QRS samplestamps
-%  maxch:       Channel with highest F1-accuracy for FQRS detections
-%  SAMPS:       Number of samples used for generating templates
-%
-% > Outputs
-%  qt_err:      Array containing QT error for each template
-%  theight_err: Array containing T-height error for each template
-%
-
-% generating reference template
-qt_err = zeros(length(residual)/SAMPS,1); theight_err = zeros(length(residual)/SAMPS,1);
-
-for j = 1:SAMPS:length(residual)
-    % checking borders
-    if j+SAMPS > length(residual)
-        endsamp = length(residual);
-    else
-        endsamp = j + SAMPS;
-    end
-    % qrs complexes in interval
-    qrstmp = fqrs(fqrs>j&fqrs<endsamp)-j;
-    % abdominal signal template
-    temp_abdm = FECGSYN_tgen(residual(j:endsamp),qrstmp);
-    % reference template
-    temp_ref = FECGSYN_tgen(fecg(j:endsamp),qrstmp);
-    temp_abdm = temp_abdm.avg; temp_ref = temp_ref.avg;
-    % evaluating morphological features
-    [qt_err(end+1),theight_err(end+1)] = FECGSYN_manalysis(temp_abdm,temp_ref);
-end
-end
 
 
 
@@ -301,47 +233,44 @@ end
 
 % % % Plot about cases
 % % if debug
-% %     % This script plots boxplots with 2 groups
-% %     % Boxplot multicolor
-% %     stats_ica(:,[1 3 4]) = 100.*stats_ica(:,[1 3 4]);
-% %     stats_tsc(:,[1 3 4]) = 100.*stats_tsc(:,[1 3 4]);
-% %     LINE_WIDTH = 1.2;
-% %     FONT_SIZE = 12;
-% %     MARKER_SIZE = 7;
-% %     fig1=figure(1)
-% %     base = cellfun(@(x) ~isempty(regexp(x, '_l\d.mat$', 'match')), fls_orig);
-% %     c0 = cellfun(@(x) ~isempty(regexp(x, '_c0.mat$', 'match')), fls_orig);
-% %     c1 = cellfun(@(x) ~isempty(regexp(x, '_c1.mat$', 'match')), fls_orig);
-% %     c2 = cellfun(@(x) ~isempty(regexp(x, '_c2.mat$', 'match')), fls_orig);
-% %     c3 = cellfun(@(x) ~isempty(regexp(x, '_c3.mat$', 'match')), fls_orig);
-% %     c4 = cellfun(@(x) ~isempty(regexp(x, '_c4.mat$', 'match')), fls_orig);
-% %     c5 = cellfun(@(x) ~isempty(regexp(x, '_c5.mat$', 'match')), fls_orig);
+% % % This script plots boxplots with 2 groups
+% % % Boxplot multicolor
+% % stats_ica(:,[1 3 4]) = 100.*stats_ica(:,[1 3 4]);
+% % stats_tsc(:,[1 3 4]) = 100.*stats_tsc(:,[1 3 4]);
+% % LINE_WIDTH = 1.2;
+% % FONT_SIZE = 12;
+% % MARKER_SIZE = 7;
+% % fig1=figure(1)
+% % base = cellfun(@(x) ~isempty(regexp(x, '_l\d.mat$', 'match')), fls_orig);
+% % c0 = cellfun(@(x) ~isempty(regexp(x, '_c0.mat$', 'match')), fls_orig);
+% % c1 = cellfun(@(x) ~isempty(regexp(x, '_c1.mat$', 'match')), fls_orig);
+% % c2 = cellfun(@(x) ~isempty(regexp(x, '_c2.mat$', 'match')), fls_orig);
+% % c3 = cellfun(@(x) ~isempty(regexp(x, '_c3.mat$', 'match')), fls_orig);
+% % c4 = cellfun(@(x) ~isempty(regexp(x, '_c4.mat$', 'match')), fls_orig);
+% % c5 = cellfun(@(x) ~isempty(regexp(x, '_c5.mat$', 'match')), fls_orig);
 % %
-% %     stats_ica = stats_ica(:,1);
-% %     stats_tsc = stats_tsc(:,1);
+% % stats_ica = stats_ica(:,1);
+% % stats_tsc = stats_tsc(:,1);
 % %
-% %     N = sum(base)+sum(c0)+sum(c1)+sum(c2)+sum(c3)+sum(c4)+sum(c5);
-% %     bh=boxplot([stats_ica(base); stats_ica(c0); stats_ica(c1); stats_ica(c2); stats_ica(c3); stats_ica(c4); stats_ica(c5);...
-% %         stats_tsc(base); stats_ica(c0); stats_tsc(c1); stats_tsc(c2); stats_tsc(c3); stats_tsc(c4); stats_tsc(c5)], ...
-% %         {[repmat({'Base'},1,sum(base)) repmat({'Case 0'},1,sum(c0)) repmat({'Case1'},1,sum(c1)) repmat({'Case2'},1,sum(c2)) repmat({'Case3'},1,sum(c3)) repmat({'Case4'},1,sum(c4)) repmat({'Case5'},1,sum(c5)) ...
-% %         repmat({'Base'},1,sum(base)) repmat({'Case 0'},1,sum(c0)) repmat({'Case1'},1,sum(c1)) repmat({'Case2'},1,sum(c2)) repmat({'Case3'},1,sum(c3)) repmat({'Case4'},1,sum(c4)) repmat({'Case5'},1,sum(c5)) ] ...
-% %         [repmat({'ICA'},N,1);repmat({'TS'},N,1)]},'colors',repmat('rk',1,6),'factorgap',10,'labelverbosity','minor','labelorientation','inline')
-% %     set(bh,'linewidth',LINE_WIDTH);
-% %     ylabel('F1 (in %)','FontSize',FONT_SIZE)
-% %     xlabel('Recording','FontSize',FONT_SIZE)
-% %     set(gca,'FontSize',FONT_SIZE)
-% %     set(findall(gcf,'type','text'),'fontSize',FONT_SIZE);
-% %     %     ylim([0 105])
-% %     xl=xlabel('Recording Number','FontSize',FONT_SIZE);
-% %     pos=get(xl,'Pos');
-% %     set(xl,'Pos',[pos(1) pos(2)-30 pos(3)])
-% %     save2pdf('boxplot',fig1,600)
+% % N = sum(base)+sum(c0)+sum(c1)+sum(c2)+sum(c3)+sum(c4)+sum(c5);
+% % bh=boxplot([stats_ica(base); stats_ica(c0); stats_ica(c1); stats_ica(c2); stats_ica(c3); stats_ica(c4); stats_ica(c5);...
+% % stats_tsc(base); stats_ica(c0); stats_tsc(c1); stats_tsc(c2); stats_tsc(c3); stats_tsc(c4); stats_tsc(c5)], ...
+% % {[repmat({'Base'},1,sum(base)) repmat({'Case 0'},1,sum(c0)) repmat({'Case1'},1,sum(c1)) repmat({'Case2'},1,sum(c2)) repmat({'Case3'},1,sum(c3)) repmat({'Case4'},1,sum(c4)) repmat({'Case5'},1,sum(c5)) ...
+% % repmat({'Base'},1,sum(base)) repmat({'Case 0'},1,sum(c0)) repmat({'Case1'},1,sum(c1)) repmat({'Case2'},1,sum(c2)) repmat({'Case3'},1,sum(c3)) repmat({'Case4'},1,sum(c4)) repmat({'Case5'},1,sum(c5)) ] ...
+% % [repmat({'ICA'},N,1);repmat({'TS'},N,1)]},'colors',repmat('rk',1,6),'factorgap',10,'labelverbosity','minor','labelorientation','inline')
+% % set(bh,'linewidth',LINE_WIDTH);
+% % ylabel('F1 (in %)','FontSize',FONT_SIZE)
+% % xlabel('Recording','FontSize',FONT_SIZE)
+% % set(gca,'FontSize',FONT_SIZE)
+% % set(findall(gcf,'type','text'),'fontSize',FONT_SIZE);
+% % % ylim([0 105])
+% % xl=xlabel('Recording Number','FontSize',FONT_SIZE);
+% % pos=get(xl,'Pos');
+% % set(xl,'Pos',[pos(1) pos(2)-30 pos(3)])
+% % save2pdf('boxplot',fig1,600)
 % %
-% %     % Plot about SNRmn
-% %     a = cellfun(@(x) strsplit(x,'_snr'), fls_orig,'UniformOutput',0);
+% % % Plot about SNRmn
+% % a = cellfun(@(x) strsplit(x,'_snr'), fls_orig,'UniformOutput',0);
 % %
-% %     b = cellfun(@(x) length(x)>1,a);
+% % b = cellfun(@(x) length(x)>1,a);
 % % end
-
-
-

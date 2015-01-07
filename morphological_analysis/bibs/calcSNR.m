8dabbb77a762458b324ddc6d9cb67c33138952e9
Fernando Andreotti <fernando.andreotti@gmx.de>
2015-01-07 14:01:56 +0100

    Added SNR pretty plots for tunning KF

--- morphological_analysis/bibs/calcSNR.m
+++ morphological_analysis/bibs/calcSNR.m
@@ -28,12 +28,7 @@
 % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
 % Public License for more details.
 %
-function [SNRfm,SNRnf] = calc_realSNR(signal,mref,fref,fs)
-%% Input Check
-if ~isa(Data,'Dataset')
-    error('Data has to be Dataset member')
-end
-
+function [SNRfm,SNRnf] = calcSNR(signal,mref,fref,fs)
 %% Parameters
 MWIN = 50;     % half of window around maternal complexes considered for signal energy (in ms)
 FWIN = 25;     % half of window length around fetal complexes (in ms)
@@ -42,16 +37,13 @@ FHR = 120; %    [in bpm]
 
 
 %% Processing
-MWIN = MWIN/(fs/1000);   % converting to samples
-FWIN = FWIN/(fs/1000);   % at fs=1000Hz does nothing
-
-% bandpass filtering signal
-ppsignal = Preprocess_fECG(signal,0,'','sampRate',fs,'bp',[10,60]);
+MWIN = round(MWIN/(1000/fs));   % converting to samples
+FWIN = round(FWIN/(1000/fs));   % at fs=1000Hz does nothing
 
 % adjusting extremities
-extremities = (mref <= MWIN | mref >= length(ppsignal)-MWIN);        % if there are peaks on the border that may lead to error
+extremities = (mref <= MWIN | mref >= length(signal)-MWIN);        % if there are peaks on the border that may lead to error
 mref = mref(~extremities);                                          % remove extremity peaks
-extremities = (fref <= FWIN | fref >= length(ppsignal)-FWIN);        % if there are peaks on the border that may lead to error
+extremities = (fref <= FWIN | fref >= length(signal)-FWIN);        % if there are peaks on the border that may lead to error
 fref = fref(~extremities);                                          % remove extremity peaks
 
 % selecting overlapping peaks
@@ -67,7 +59,7 @@ m_over(cell2mat(idxover)) = true;           % mat. indices with overlap
 m_nover = ~m_over;                       % mat. indices with no overlap
 
 % defining limits for energy regions
-sigtime = false(1,Data.length);
+sigtime = false(1,length(signal));
 mat_sig = sigtime; mat_sig(mref(m_nover)) = true;          % creates a maternal allowed parts
 mat_sig = logical(filter(ones(1,2*MWIN),1,mat_sig));       % maternal useful signal
 mat_sig = [mat_sig(MWIN+1:end) false(1,MWIN)];               % taking moving average window delay
@@ -91,9 +83,9 @@ noise_sig = ~(mat_sig + mat_nosig + fet_sig + fet_nosig);
 mbeats = 60*fs*length(mref)/length(signal); % now im bpm
 fbeats = 60*fs*length(fref)/length(signal); % now im bpm
 
-Pmat = sqrt(sum(ppsignal(:,mat_sig).^2,2))/sum(m_nover); % maternal power (in each channel)
+Pmat = sqrt(sum(signal(:,mat_sig).^2,2))/sum(m_nover); % maternal power (in each channel)
 Pmat = Pmat.*(MHR/mbeats);                          % normalized power
-Pfet = sqrt(sum(ppsignal(:,fet_sig).^2,2))/sum(f_nover); % fetal power (in each channel)
+Pfet = sqrt(sum(signal(:,fet_sig).^2,2))/sum(f_nover); % fetal power (in each channel)
 Pfet = Pfet.*(FHR/fbeats);                          % normalized power
 Pn   = sqrt(sum(signal(:,noise_sig).^2,2))/sum(m_nover); % noise power (in each channel)
 
@@ -101,18 +93,22 @@ SNRfm = 10*log10(Pfet./Pmat);
 SNRnf = 10*log10(Pn./Pfet);
 
 %% Plotting
-% chan = 1; % channel to plot
-% 
-% t = 1:Data.length;
-% plot(signal(chan,:),'Color',[0.5, 0.5, 0.5])
-% hold on
-% mref_plot = NaN(1,length(fet_sig));
-% fref_plot = NaN(1,length(fet_sig));
-% mref_plot(mref) = 0.3;                   % height of refs
-% fref_plot(fref) = 0.3;
-% plot(mref_plot,'dg','MarkerSize',7)      % maternal ref
-% plot(fref_plot,'xr','MarkerSize',7)      % fetal ref
-% 
+figure
+hold on
+chan = 1; % channel to plot
+maxes = max(abs(signal)')
+t = 1:length(signal);
+marea = area(t',maxes(chan)*[-mat_sig',2*mat_sig'],'FaceColor',[156 177 219]./255,'EdgeColor','none');
+farea = area(t',maxes(chan)*[-fet_sig',2*fet_sig'],'FaceColor',[187 142 187]./255,'EdgeColor','none');
+narea = area(t',maxes(chan)*[-noise_sig',2*noise_sig'],'FaceColor',[0.9 0.9 0.9],'EdgeColor','none');
+alpha = 0.1;
+plot(signal(chan,:),'Color',[0.7, 0.7, 0.7])
+
+xlabel('Time(s)','FontSize',14,'FontWeight','bold'), ylabel('Amplitude (mV)','FontSize',14,'FontWeight','bold')
+set(gca,'XTick',1:fs:length(signal))  % This automatically sets
+set(gca,'XTickLabel',num2cell(0:1:length(signal)/fs))
+set(gca,'FontSize',12)
+
 % % for fast plot substituting logical to NaN
 % mat_plot = NaN(1,length(mat_sig));    
 % mat_plot(mat_sig) =  4*median(signal(chan,mat_sig));
@@ -127,8 +123,4 @@ SNRnf = 10*log10(Pn./Pfet);
 % plot(noise_plot,'b');plot(-noise_plot,'b');          % noise blocks
 % 
 % legend('preproc. Signal', 'mat. Reference','fet. Reference')
-% xlabel('Time(s)','FontSize',14,'FontWeight','bold'), ylabel('Amplitude (mV)','FontSize',14,'FontWeight','bold')
-% set(gca,'XTick',1:1000:Data.length)  % This automatically sets
-% set(gca,'XTickLabel',num2cell(0:1:Data.length/1000))
-% set(gca,'FontSize',12)
 

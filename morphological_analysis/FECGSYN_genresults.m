e1f8203872c834a6e902aa5f6b6ffd7eba364d72
andreotti-lp <fernando.andreotti@gmx.de>
2015-08-10 18:22:14 +0200

    Added rsquared

--- morphological_analysis/FECGSYN_genresults.m
+++ morphological_analysis/FECGSYN_genresults.m
@@ -351,7 +351,8 @@ if ~exp3 % Experiment 2
 else
     %% Experiment 3
     
-    %= Distribution of ICA FQT intervals
+    %= Distribution of ICA
+    % FQT intervals
     exp3dist = exp3dist1;
     nans = sum(cellfun(@(x) sum(sum(isnan(cell2mat(x)))),exp3dist));
     tot = sum(cellfun(@(x) numel(cell2mat(x)),exp3dist));
@@ -367,10 +368,58 @@ else
     end
     fprintf('NaNs on source domain:  %3.2f percent \n',srcnan/srctot*100);
     fprintf('NaNs on time domain:  %3.2f percent \n',timenan/timetot*100);
-    scatter(exp3med(:,2),exp3med(:,1),12,'filled')
+    x = exp3med(:,2); y = exp3med(:,1);
+    scatter(x,y,12,'filled')
+    % regression
+    A = [x,y];
+    A(any(isnan(A), 2),:)=[];
+    x = A(:,1); y = A(:,2);
+    p = polyfit(x,y,1);
+    yfit = polyval(p,x);
+    yresid = y - yfit;
+    SSresid = sum(yresid.^2);
+    SStotal = (length(y)-1) * var(y);
+    rsq = 1 - SSresid/SStotal;
+    hold on; plot(x,yfit,'r-'); 
+    text(220,140,sprintf('r^2 = %2.4f',rsq))
+    
     xlim([120,260]),ylim([120,260])
     xlabel('FQT in time domain (ms)')
     ylabel('FQT in source domain (ms)')
+    
+    % FTh
+    exp3dist = exp3dist1;
+    nans = sum(cellfun(@(x) sum(sum(isnan(cell2mat(x)))),exp3dist));
+    tot = sum(cellfun(@(x) numel(cell2mat(x)),exp3dist));
+    srcnan = nans(1); srctot = tot(1);
+    timenan = nans(2); timetot = tot(2);
+    exp3med = cellfun(@(x) nanmedian(abs(cell2mat(x))),exp3dist,'UniformOutput',0);
+    exp3med = [cell2mat(exp3med(:,3)')' cell2mat(exp3med(:,4)')'];
+    [h,p]=ttest(exp3med(:,2),exp3med(:,1));
+    if h
+        fprintf('Null hypothesis can be rejected with p= %d \n',p);
+    else
+        fprintf('Null hypothesis CANNOT be rejected');
+    end
+    fprintf('NaNs on source domain:  %3.2f percent \n',srcnan/srctot*100);
+    fprintf('NaNs on time domain:  %3.2f percent \n',timenan/timetot*100);
+     x = exp3med(:,2); y = exp3med(:,1);
+    scatter(x,y,12,'filled')
+    % regression
+    A = [x,y];
+    A(any(isnan(A), 2),:)=[];
+    x = A(:,1); y = A(:,2);
+    p = polyfit(x,y,1);
+    yfit = polyval(p,x);
+    yresid = y - yfit;
+    SSresid = sum(yresid.^2);
+    SStotal = (length(y)-1) * var(y);
+    rsq = 1 - SSresid/SStotal;
+    hold on; plot(x,yfit,'r-'); 
+    text(1.5,0.1,sprintf('r^2 = %2.4f',rsq))
+   % xlim([120,260]),ylim([120,260])
+    xlabel('FT_h in time domain (n.u.)')
+    ylabel('FT_h in source domain (n.u.)')
     
     % Case by case methods against each other
     % Generate Table

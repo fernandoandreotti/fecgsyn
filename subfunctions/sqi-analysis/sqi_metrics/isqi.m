4eb383f6d5c837a7d08f76265910f8082a2787ba
FernandoLP <fernando.andreotti@gmx.de>
2017-03-16 08:42:20 +0000

    Updating headers

--- subfunctions/sqi-analysis/sqi_metrics/isqi.m
+++ subfunctions/sqi-analysis/sqi_metrics/isqi.m
@@ -28,14 +28,42 @@ function sqi = isqi(qrs,win,fs)
 % Johnson, A. E. W., Behar, J., Andreotti, F., Clifford, G. D. and Oster, J.
 %
 %
-% This program is free software; you can redistribute it and/or modify it
-% under the terms of the GNU General Public License as published by the
-% Free Software Foundation; either version 2 of the License, or (at your
-% option) any later version.
-% This program is distributed in the hope that it will be useful, but
-% WITHOUT ANY WARRANTY; without even the implied warranty of
-% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
-% Public License for more details.
+% --
+% fecgsyn toolbox, version 1.2, March 2017
+% Released under the GNU General Public License
+%
+% Copyright (C) 2017  Joachim Behar & Fernando Andreotti
+% Department of Engineering Science, University of Oxford
+% joachim.behar@oxfordalumni.org, fernando.andreotti@eng.ox.ac.uk
+%
+% 
+% For more information visit: http://www.fecgsyn.com
+% 
+% Referencing this work
+% (SQI indices)
+% Andreotti, F., Gräßer, F., Malberg, H., & Zaunseder, S. (2017). Non-Invasive Fetal ECG Signal Quality Assessment for 
+% Multichannel Heart Rate Estimation. IEEE Trans. Biomed. Eng., (in press).
+%  
+% (FECGSYN Toolbox)
+% Behar, J., Andreotti, F., Zaunseder, S., Li, Q., Oster, J., & Clifford, G. D. (2014). An ECG Model for Simulating 
+% Maternal-Foetal Activity Mixtures on Abdominal ECG Recordings. Physiol. Meas., 35(8), 1537–1550.
+% 
+%
+% Last updated : 15-03-2017
+% 
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% 
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 %
 
 % == managing inputs
--- subfunctions/sqi-analysis/sqi_metrics/xsqi.m
+++ subfunctions/sqi-analysis/sqi_metrics/xsqi.m
@@ -16,23 +16,41 @@ function sqi = xsqi(signal,qrs,fs,win)
 % Output:
 %   sqi:            resulting xSQI for segment
 % 
-% Fetal Extraction Toolbox, version 1.0, February 2014
+% --
+% fecgsyn toolbox, version 1.2, March 2017
 % Released under the GNU General Public License
 %
-% Copyright (C) 2014 Fernando Andreotti
-% Dresden University of Technology, Institute of Biomedical Engineering
-% fernando.andreotti@mailbox.tu-dresden.de
+% Copyright (C) 2017  Joachim Behar & Fernando Andreotti
+% Department of Engineering Science, University of Oxford
+% joachim.behar@oxfordalumni.org, fernando.andreotti@eng.ox.ac.uk
 %
-% Last updated : 30-06-2016
+% 
+% For more information visit: http://www.fecgsyn.com
+% 
+% Referencing this work
+% (SQI indices)
+% Andreotti, F., Gräßer, F., Malberg, H., & Zaunseder, S. (2017). Non-Invasive Fetal ECG Signal Quality Assessment for 
+% Multichannel Heart Rate Estimation. IEEE Trans. Biomed. Eng., (in press).
+%  
+% (FECGSYN Toolbox)
+% Behar, J., Andreotti, F., Zaunseder, S., Li, Q., Oster, J., & Clifford, G. D. (2014). An ECG Model for Simulating 
+% Maternal-Foetal Activity Mixtures on Abdominal ECG Recordings. Physiol. Meas., 35(8), 1537–1550.
+% 
 %
-% This program is free software; you can redistribute it and/or modify it
-% under the terms of the GNU General Public License as published by the
-% Free Software Foundation; either version 2 of the License, or (at your
-% option) any later version.
-% This program is distributed in the hope that it will be useful, but
-% WITHOUT ANY WARRANTY; without even the implied warranty of
-% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
-% Public License for more details.
+% Last updated : 15-03-2017
+% 
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% 
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 %% Generate a template
